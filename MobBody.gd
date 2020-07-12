f75a19201784c0fa640e0281e03aff40ae02153c
uvehj <49170460+uvehj@users.noreply.github.com>
2020-07-12 09:57:00 +0200

    no time

--- MobBody.gd
+++ MobBody.gd
@@ -6,6 +6,8 @@ var faction
 var color = Color(0.0,0.0,0.0)
 var collisionShape = CollisionShape.new()
 var shape_owner
+var swarm = true
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,8 +15,17 @@ func _ready():
 	var shape = CircleShape2D.new()
 	shape.radius= size
 	shape_owner_add_shape(shape_owner, shape)
+	rng.randomize()
 func _draw():
-	draw_circle(Vector2(0,0),size,color)
+	if !swarm:
+		draw_circle(Vector2(0,0),size,color)
+	elif size > 0:
+		for i in PI * pow(size, 2) / 100:
+			var r = size * sqrt(rng.randf_range(0.0,1.0))
+			var t = rng.randf_range(0.0,1.0) * 2 * PI
+			var x = r * cos(t)
+			var y = r * sin(t)
+			draw_circle(Vector2(x,y), 2, color)
 	
 func set_size(radius):
 	size=radius
--- mob.gd
+++ mob.gd
@@ -3,14 +3,24 @@ extends Node2D
 
 export var faction = "neutral"
 export var size = 10
-var combat_speed = 5
+var combat_speed = 10
 var objective = Vector2(0,0)
+var action = "stay"
+var mandated_action = "stay"
+var movement_speed = 20
+var rng = RandomNumberGenerator.new()
+
+const max_x = 1280
+const max_y = 720
+const min_x = 0
+const min_y = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MobBody.set_size(size)
 	$MobBody.faction=faction
 	self.set_color()
+	rng.randomize()
 	
 func set_color():
 	if faction == "rebels":
@@ -18,7 +28,7 @@ func set_color():
 	elif faction == "monarch":
 		$MobBody.color=Color(0.0,0.0,1.0)
 	elif faction == "church":
-		$MobBody.color=Color(1.0,1.0,0.0)
+		$MobBody.color=Color(0.5,0.5,0.0)
 	else:
 		$MobBody.color=Color(0.0,0.0,0.0)
 
@@ -28,6 +38,21 @@ func _process(delta):
 		if area.name == "MobBody":
 			self.mob_clash(area, delta)
 	self.act()
+	if action == "move" :
+		var velocity=(objective - self.global_position).normalized() * movement_speed * delta
+		translate(velocity)
+	if action == "flee" :
+		var velocity=(self.global_position - objective).normalized() * movement_speed * delta
+		translate(velocity)
+	
+	if self.global_position.x < min_x:
+		self.global_position.x = min_x
+	if self.global_position.y < min_y:
+		self.global_position.y = min_y
+	if self.global_position.x > max_x:
+		self.global_position.x = max_x
+	if self.global_position.y > max_y:
+		self.global_position.y = max_y
 
 func mob_clash(clash_area, delta):
 	var clash_faction = clash_area.faction
@@ -37,6 +62,8 @@ func mob_clash(clash_area, delta):
 				size = size - 1 * delta * combat_speed
 			else:
 				size = size + 1 * delta * combat_speed
+		if faction == "neutral":
+			size = size - 1 * delta * combat_speed
 	if clash_faction == "rebels":
 		if faction == "neutral":
 			size = size - 1 * delta * combat_speed
@@ -60,16 +87,54 @@ func act():
 		self.church_actions()
 	if faction == "monarch":
 		self.monarch_actions()
-	if faction == "rebels":
-		self.rebels_actions()
 	if faction == "neutral":
 		self.neutral_actions()
 
 func church_actions():
-	pass
+	action = "stay"
+	objective = self.global_position
+	var overlapping_area_list = $LineOfSight.get_overlapping_areas()
+	var enemies_close = false
+	for area in overlapping_area_list:
+		if area.name == "MobBody":
+			if area.faction == "rebels":
+				enemies_close = true
+				if area.size > self.size:
+					action = "flee"
+					objective = area.global_position
+				if area.size <= self.size:
+					if action != "flee" and action =="stay":
+						action = "move"
+						objective = area.global_position
+					elif action != "flee": 
+						if self.global_position.distance_to(objective) > self.global_position.distance_to(area.global_position):
+							objective = area.global_position
+							action = "move"
+			if area.faction == "neutral" and !enemies_close:
+				if action =="stay":
+					action = "move"
+					objective = area.global_position
+				else:
+					if self.global_position.distance_to(objective) > self.global_position.distance_to(area.global_position):
+						objective = area.global_position
+	if action == "stay":
+		action = "move"
+		if objective.distance_to(self.global_position) < movement_speed or objective.distance_to(Vector2(0,0)) == 0:
+			objective = Vector2(rng.randi_range(min_x,max_x),rng.randi_range(min_y,max_y))
 func monarch_actions():
-	pass
-func rebels_actions():
-	pass
+	action = "stay"
+	objective = self.global_position
+	var overlapping_area_list = $LineOfSight.get_overlapping_areas()
+	for area in overlapping_area_list:
+		if area.name == "MobBody":
+			if area.faction == "rebels":
+				if action =="stay":
+					action = "move"
+					objective = area.global_position
+				else:
+					if self.global_position.distance_to(objective) > self.global_position.distance_to(area.global_position):
+						objective = area.global_position
 func neutral_actions():
-	pass
+	action = "move"
+	if objective.distance_to(self.global_position) < movement_speed or objective.distance_to(Vector2(0,0)) == 0:
+		objective = Vector2(rng.randi_range(min_x,max_x),rng.randi_range(min_y,max_y))
